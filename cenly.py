137c8c2270d687009d270c8b8c5578ff613b75b8
teddapoet <mingwongle@gmail.com>
2025-07-16 22:10:43 -0700

    stop tracking .db files

--- cenly.py
+++ cenly.py
@@ -11,25 +11,21 @@ from scripts import bot
 from rag import vector_stores
 
 
-
-# Chat message history graph creation 
-workflow = StateGraph(state_schema=MessagesState)
-
 def call_model(state: MessagesState):
     messages = state["messages"]        # get all past messages + current message
     question = messages[-1].content
 
-    #checking conversation history
-    # for msg in messages:                                      
-    #     role = getattr(msg, "type")
-    #     if role == "system":
-    #         print(f"[System]: {msg.content}")
-    #     elif role == "human":
-    #         print(f"[me]: {msg.content}")
-    #     elif role == "ai" or role == "AIMessage":
-    #         print(f"[Cenly]: {msg.content}")
-    #     else:
-    #         print(f"[{role}]: {msg.content}")
+    checking conversation history
+    for msg in messages:                                      
+        role = getattr(msg, "type")
+        if role == "system":
+            print(f"[System]: {msg.content}")
+        elif role == "human":
+            print(f"[me]: {msg.content}")
+        elif role == "ai" or role == "AIMessage":
+            print(f"[Cenly]: {msg.content}")
+        else:
+            print(f"[{role}]: {msg.content}")
 
 
     # # Mock data loading
@@ -47,15 +43,21 @@ def call_model(state: MessagesState):
     retriever = vector_store.as_retriever(search_type='mmr', search_kwargs = {'k' : 3, 'fetch_k' : 10, 'lambda_mult': 1})
     docs = retriever.invoke(question)
     context = "\n\n".join([doc.page_content for doc in docs])
-    print(context)
+    # print(context)
 
     response = AIMessage(bot.cenly_analyze(context, question, messages))
     return {"messages": [response]} 
 
 
-workflow.add_edge(START, "chatbot")
-workflow.add_node("chatbot", call_model)
-workflow.add_edge("chatbot", END)
+# Chat message history graph creation 
+workflow = StateGraph(state_schema=MessagesState)
+
+def workflow_setup():
+    workflow.add_edge(START, "chatbot")
+    workflow.add_node("chatbot", call_model) 
+    workflow.add_edge("chatbot", END)
+
+workflow_setup()
 
 # Database context window
 with SqliteSaver.from_conn_string("ted_chat_history.db") as memory:
